4d5447b58677eed7438b621f306bff1d5994f2d9
alaminhossain.web-tec <alaminhossain1997.web@gmail.com>
2026-05-22 03:52:01 +0600

    update

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,8 +1,13 @@
 const { otpmailTemplate } = require("../helpers/emailTemplate");
 const { mailsender } = require("../helpers/mailService");
-const { isvalidEmail, isvalidPassword, generateOTP } = require("../helpers/utils");
+const {
+  isvalidEmail,
+  isvalidPassword,
+  generateOTP,
+} = require("../helpers/utils");
 const userSchema = require("../models/userSchema");
 
+// registration/SignUp
 const registration = async (req, res) => {
   try {
     const { fullName, email, password } = req.body;
@@ -19,30 +24,89 @@ const registration = async (req, res) => {
       return res
         .status(400)
         .send({ message: "Password is required", field: "password" });
+
     if (!isvalidEmail(email))
       return res.status(400).send({ message: "Invalid Email", field: "email" });
+
     if (!isvalidPassword(password))
       return res
         .status(400)
         .send({ message: "Invalid Password", field: "password" });
+
     const existingEmail = await userSchema.findOne({ email });
     if (existingEmail)
       return res
         .status(400)
         .send({ message: "This Email already exist", field: "email" });
+
     const otp = generateOTP();
-    const  otpExpiary =  Date.now()+5*60*1000
-        await userSchema.create({fullName, email, password, otp, otpExpiary})
-        await mailsender({
-          email,
-          subject: "Verify your OTP",
-          template: otpmailTemplate(otp, otpExpiary),
-        });
-        return res.status(201).send({ message: "Registration successful, verify your email" });
+    const otpExpiary = Date.now() + 5 * 60 * 1000;
+    await userSchema.create({ fullName, email, password, otp, otpExpiary });
+    await mailsender({
+      email,
+      subject: "Verify your OTP",
+      template: otpmailTemplate(otp, otpExpiary),
+    });
+    return res
+      .status(201)
+      .send({ message: "Registration successful, verify your email" });
   } catch (error) {
     console.log(error);
     return res.status(500).send({ message: "Internal Server error!" });
   }
 };
 
-module.exports = { registration };
+//otp verification
+const verifyOtp = async (req, res) => {
+  const { email, otp } = req.body;
+  try {
+
+    if (!email) return res.status(400).send({ message: "Email is required !" });
+    if (!otp) return res.status(400).send({ message: "Otp code is required !" });
+
+    const userData = await userSchema.findOne({ email, isvarified: false });
+
+    if (!userData)
+       return res.status(400).send({ message: "User not found" });
+    if (userData.otp != otp)
+      return res.status(400).send({ message: "Invalid Otp" });
+    if (userData.otpExpiary < Date.now())
+      return res.status(400).send({ message: "Otp Expired" });
+
+    userData.otp = null;
+    userData.isvarified = true;
+    userData.otpExpiary = null;
+    await userData.save();
+
+    res.status(200).send({ message: "Email verification successfull" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: "email verification failed" });
+  }
+};
+
+//resend otp 
+const resendOtp = async (req,res) =>{
+  const {email} = req.body;
+  try {
+    const userData = await userSchema.findOne({email, isvarified:false});
+    if (!userData) return res.status(400).send({message:"User not found"});
+    
+    const otp = generateOTP();
+    userData.otp= otp
+    userData.otpExpiary = Date.now() + 5 * 60 * 1000;
+    await userData.save()
+    await mailsender({
+      email,
+      subject: "Verify your OTP",
+      template: otpmailTemplate(otp, userData.otpExpiary),
+    });
+
+    res.status(200).send({message: "New OTP send to your email"})
+
+  } catch (error) {
+    res.status(400).send({message:"Failed to resend OTP !"})
+  }
+}
+
+module.exports = { registration , verifyOtp, resendOtp};
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const route = express.Router();
 
-const { registration } = require("../controllers/authControllers");
+const { registration, verifyOtp, resendOtp } = require("../controllers/authControllers");
 
 route.post("/registration", registration);
+route.post("/verifyotp", verifyOtp);
+route.post("/resendotp", resendOtp);
 
 module.exports = route;
